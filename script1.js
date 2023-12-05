4c4bf861b5e6d0dbdec7c47cac75619cc983d359
Doljana <145448894+Doljana@users.noreply.github.com>
2023-12-05 15:35:18 +0100

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,49 @@
+
+
+
+// const newElement = document.createElement('div');
+
+// newElement.textContent ;
+
+// const existingElement = document.getElementById('existingElement');
+// existingElement.parentNode.insertBefore(newElement, existingElement.nextSibling);
+
+
+
+
+
+// newChildElement.textContent ;
+
+
+// const parentElement = document.getElementById('parentElement');
+// parentElement.appendChild(newChildElement);
+
+
+// const elementToRemove = document.querySelector('.removeMe');
+// elementToRemove.parentNode.removeChild(elementToRemove);
+// const products = [
+//   { title: 'Товар 1', unit_price: 10, count: 3 },
+//   { title: 'Товар 2', unit_price: 15, count: 2 },
+//   { title: 'Товар 3', unit_price: 20, count: 1 }
+// ];
+
+
+// const productsContainer = document.getElementById('productsContainer');
+
+
+// let totalSum = 0;
+// let totalCount = 0;
+
+// products.forEach(product => {
+//   const card = document.createElement('div');
+//   card.innerHTML = `<p>${product.title}</p><p>Цена: ${product.unit_price}</p><p>Количество: ${product.count}</p>`;
+//   productsContainer.appendChild(card);
+
+//   totalSum += product.unit_price * product.count;
+//   totalCount += product.count;
+// });
+
+
+// const totalInfo = document.createElement('div');
+// totalInfo.innerHTML = `<p>Итого: ${totalSum}</p><p>Количество товаров: ${totalCount}</p>`;
+// productsContainer.appendChild(totalInfo);
